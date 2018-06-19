d2061a791b7c5279f4476eb9757fb5575fc3dfd3
john argote <argote.john@gmail.com>
2018-06-19 17:33:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens = ['Milo','Otis','Garfield'];
+
+function destructivelyAppendKitten() {
+  return kittens.push('Ralph');
+}
+
+function destructivelyPrependKitten() {
+  return kittens.unshift('Bob');
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten() {
+  return [...kittens,'Broom'];
+}
+
+function prependKitten() {
+  return ['Arnold',...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0,kittens.length(-2));
+}
